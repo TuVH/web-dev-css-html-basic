6eab33958d57d1c38a823cd7fe15b510df018c7f
Tu <hoangtu1392001@gmail.com>
2020-10-20 21:00:52 +0700

    [Add]_File Exercise4b20-10

---
+++ ConsoleApp/Exercise4b20-10/Funtion.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Funtion
+    {
+        private int[] arr;
+        public void Check()
+        {
+            int iNumber = 0;
+            while (iNumber <= 0)
+            {
+                Console.Write("Input length of array :");
+                int.TryParse(Console.ReadLine(), out iNumber);
+                arr = new int[iNumber];
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements;
+                Console.Write("input array[{0}]:", i);
+
+                bool success = int.TryParse(Console.ReadLine(), out elements);
+                while (!success)
+                {
+                    Console.Write("Wrong input! , try again!,input array[{0}]:", i);
+                    success = int.TryParse(Console.ReadLine(), out elements);
+                }
+                arr[i] = elements;
+            }
+        }
+        public bool CheckContainNumber5()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool CheckContainNumber6()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 6)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool CheckContainNumber5Or6()
+        {
+            if (CheckContainNumber5() || CheckContainNumber6())
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool CheckContainNumber5And6()
+        {
+            if (CheckContainNumber5() && CheckContainNumber6())
+            {
+                return true;
+            }
+            return false;
+        }
+        public int CountNumber5()
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] ==5 )
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public int CountNumber5Or6()
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5 || arr[i] ==6)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ ConsoleApp/Exercise4b20-10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercise 4b
+            Funtion funtion = new Funtion();
+            // input and Check elements in array.
+            funtion.Check();
+
+            // Check if array has element = 5
+            Console.WriteLine("Array has element = 5 :"+ funtion.CheckContainNumber5());
+
+            // Check if array has element = 5 Or element = 6
+            Console.WriteLine("Array has element = 6 :"+ funtion.CheckContainNumber5Or6());
+
+            // Check if array has both element 5 and 6
+            Console.WriteLine("Array has element = 6 and element = 5 :"
+                + funtion.CheckContainNumber5And6());
+            // Count number 5 in array.
+            Console.WriteLine("Quantity of elements = 5 in Array :" + funtion.CountNumber5());
+
+            // Count number 5 or number 6 in array.
+            Console.WriteLine("Quantity of elements = 5 Or elements = 6 in Array :" 
+                + funtion.CountNumber5Or6());
+
+            //caculate the sum of array.
+            Console.WriteLine("Sum of all elements in array :" +funtion.Sum());
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
