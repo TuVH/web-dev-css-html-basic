f5f25e43f35ec8f5790ef851afb4474d15501a59
Tu <hoangtu1392001@gmail.com>
2020-10-31 09:54:01 +0700

    [Add]_File WinForm C#

---
+++ ConsoleApp/Exercise6/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Account
+    {
+        private int money;
+
+        public int Money { get => money; set => money = value; }
+
+        public Account()
+        {
+        }
+
+        public Account(int money)
+        {
+            this.money = money;
+        }
+
+        public string Withdraw(int amount)
+        {
+            if (amount > money)
+            {
+                return "The account balance is not enough to make the transaction";
+            }
+            money -= amount;
+            return "Transaction OK";
+        }
+        public void Deposit(int amount)
+        {
+            money += amount;
+            Console.WriteLine("Successful!");
+        }
+        public int Balance()
+        {
+            return money;
+        }
+    }
+}
---
+++ ConsoleApp/Exercise6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account tu = new Account();
+            tu.Money =0;
+            Console.WriteLine("My money:" + tu.Balance());
+            tu.Deposit(5000000);
+            Console.WriteLine("My money:" + tu.Balance());
+            Console.WriteLine(tu.Withdraw(4000000));
+            Console.WriteLine("My money:" + tu.Balance());
+
+           
+            
+
+            Console.ReadKey();
+        }
+    }
+}
