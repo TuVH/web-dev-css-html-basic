1d4da9ad5d696c880011937d55b0644a55c3d60c
Tu <hoangtu1392001@gmail.com>
2020-11-03 21:58:06 +0700

    [Modify]_File

---
+++ ConsoleApp/Test/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Note  
+    {
+        public object data;        
+        public Note Next;
+        public Note(object data)
+        {
+            this.data = data;
+        }
+    }
+}
