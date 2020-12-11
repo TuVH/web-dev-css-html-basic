5b0fbbc16a8a765cf349766f8b6eb6a6d8376577
Tu <hoangtu1392001@gmail.com>
2020-12-11 22:59:04 +0700

    [Add]_Cake_Management_Project

---
+++ ConsoleApp/Bakery_197PM33774/FormCake.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Bakery_197PM33774
+{
+    partial class Frm_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgBakery = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aDDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dELETEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgBakery)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtgBakery
+            // 
+            this.dtgBakery.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgBakery.Location = new System.Drawing.Point(1, 27);
+            this.dtgBakery.Name = "dtgBakery";
+            this.dtgBakery.Size = new System.Drawing.Size(824, 327);
+            this.dtgBakery.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aDDToolStripMenuItem,
+            this.eDITToolStripMenuItem,
+            this.dELETEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(825, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aDDToolStripMenuItem
+            // 
+            this.aDDToolStripMenuItem.Name = "aDDToolStripMenuItem";
+            this.aDDToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.aDDToolStripMenuItem.Text = "ADD";
+            this.aDDToolStripMenuItem.Click += new System.EventHandler(this.aDDToolStripMenuItem_Click);
+            // 
+            // eDITToolStripMenuItem
+            // 
+            this.eDITToolStripMenuItem.Name = "eDITToolStripMenuItem";
+            this.eDITToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.eDITToolStripMenuItem.Text = "EDIT";
+            this.eDITToolStripMenuItem.Click += new System.EventHandler(this.eDITToolStripMenuItem_Click);
+            // 
+            // dELETEToolStripMenuItem
+            // 
+            this.dELETEToolStripMenuItem.Name = "dELETEToolStripMenuItem";
+            this.dELETEToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.dELETEToolStripMenuItem.Text = "DELETE";
+            this.dELETEToolStripMenuItem.Click += new System.EventHandler(this.dELETEToolStripMenuItem_Click);
+            // 
+            // Frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 355);
+            this.Controls.Add(this.dtgBakery);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Frm_main";
+            this.Text = "Cake Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgBakery)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgBakery;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aDDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eDITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dELETEToolStripMenuItem;
+    }
+}
+
---
+++ ConsoleApp/Bakery_197PM33774/FormEditCake.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Bakery_197PM33774
+{
+    partial class Frm_edit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cbb_kindName = new System.Windows.Forms.ComboBox();
+            this.txt_cakeName = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.lb_cakeName = new System.Windows.Forms.Label();
+            this.lb_kindName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Cbb_kindName
+            // 
+            this.Cbb_kindName.FormattingEnabled = true;
+            this.Cbb_kindName.Location = new System.Drawing.Point(158, 76);
+            this.Cbb_kindName.Name = "Cbb_kindName";
+            this.Cbb_kindName.Size = new System.Drawing.Size(371, 21);
+            this.Cbb_kindName.TabIndex = 15;
+            // 
+            // txt_cakeName
+            // 
+            this.txt_cakeName.Location = new System.Drawing.Point(158, 26);
+            this.txt_cakeName.Name = "txt_cakeName";
+            this.txt_cakeName.Size = new System.Drawing.Size(371, 20);
+            this.txt_cakeName.TabIndex = 14;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(364, 119);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(84, 35);
+            this.btn_save.TabIndex = 13;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(186, 119);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(92, 35);
+            this.btn_cancel.TabIndex = 12;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb_cakeName
+            // 
+            this.lb_cakeName.AutoSize = true;
+            this.lb_cakeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_cakeName.Location = new System.Drawing.Point(33, 26);
+            this.lb_cakeName.Name = "lb_cakeName";
+            this.lb_cakeName.Size = new System.Drawing.Size(96, 20);
+            this.lb_cakeName.TabIndex = 11;
+            this.lb_cakeName.Text = "CakeName";
+            // 
+            // lb_kindName
+            // 
+            this.lb_kindName.AutoSize = true;
+            this.lb_kindName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_kindName.Location = new System.Drawing.Point(38, 79);
+            this.lb_kindName.Name = "lb_kindName";
+            this.lb_kindName.Size = new System.Drawing.Size(88, 20);
+            this.lb_kindName.TabIndex = 10;
+            this.lb_kindName.Text = "Kindname";
+            // 
+            // Frm_edit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Cbb_kindName);
+            this.Controls.Add(this.txt_cakeName);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.lb_cakeName);
+            this.Controls.Add(this.lb_kindName);
+            this.Name = "Frm_edit";
+            this.Text = "Edit Cake";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Cbb_kindName;
+        private System.Windows.Forms.TextBox txt_cakeName;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Label lb_cakeName;
+        private System.Windows.Forms.Label lb_kindName;
+    }
+}
